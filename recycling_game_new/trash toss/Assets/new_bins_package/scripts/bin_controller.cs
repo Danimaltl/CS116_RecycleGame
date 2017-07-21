4771ea30e92c3789b0393181ff2710a3fd103908
ryankwok <rfkwok@ucsc.edu>
2017-07-21 00:20:41 -0700

    Blinking Complete
    Still need to animate the rest though

---
+++ recycling_game_new/trash toss/Assets/new_bins_package/scripts/bin_controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bin_controller : MonoBehaviour {
+
+	float currentMood = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//  Make sure the mood is a legal mood, to be safe
+		currentMood = capMood (currentMood);
+
+	}
+
+	public int capMood( float rawMood ){
+		//  return an int capped at 0 and 10
+		return (int)Mathf.Round(Mathf.Max(0, Mathf.Min(10, rawMood)));
+	}
+}
